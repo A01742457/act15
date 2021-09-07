afe9c506e3f501aefc7b2a126460442a45cac283
A01742457 <a01742457@tec.mx>
2021-09-07 14:44:58 +0000

    cuenta numeros

---
+++ assignments/03SumaNNumeros/src/exercise.py
@@ -0,0 +1,21 @@
+def suman(n):
+    suma = 0
+
+    for i in range (1, n +1):
+        suma = suma + i
+    
+    return suma 
+
+def main():
+    #escribe tu código abajo de esta línea
+    
+    n = int(input("Dame un número: "))
+
+    suma = suman(n)
+
+    print(suma) 
+
+
+
+if __name__=='__main__':
+    main()
